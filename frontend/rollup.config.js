af9276f8da7069aa9a1cf6a9d4b76772fee0bf78
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-05-20 21:19:24 +0700

    add plugin-replace for getting env value

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -1,11 +1,12 @@
-import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
+import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import livereload from 'rollup-plugin-livereload';
+import svelte from 'rollup-plugin-svelte';
+import { terser } from 'rollup-plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -57,7 +58,6 @@ export default {
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
-
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration -
@@ -71,6 +71,10 @@ export default {
 		typescript({
 			sourceMap: !production,
 			inlineSources: !production
+		}),
+		//replace
+		replace({
+			'process.env.NODE_ENV': process.env.NODE_ENV === "production" ? "'production'" : "'development'"
 		}),
 
 		// In dev mode, call `npm run start` once
